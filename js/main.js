b426e70b4bdea8a27c9dd17c2cd417d24499db8b
Sebastian Śliwiński <sxsliwinski@gmail.com>
2018-03-21 20:45:25 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-document.getElementById('oblicz').addEventListener('click', function(){
+document.getElementById('oblicz').addEventListener('click', function () {
+    var uczen = document.getElementsByClassName('uczen');
     var tytul = document.querySelectorAll('[type="number"]');
-
-    for(var i = 0; i < tytul.length; i++){
-        var sredniaOcen = tytul[i].defaultValue;
-        console.log(sredniaOcen);
-        document.getElementsByClassName('srednia')[i].innerHTML = sredniaOcen;
-    }
+    var wynik = 0;
+    var dodatkowe = 0.5;
     
+        for (var i = 0; i < 7; i++) {
+            var liczba = parseFloat(tytul[i].value);
+            var srednia = (wynik += liczba) / 7;
+        }
+        if (document.getElementsByClassName('zajecia-dodatkowe') !== '' && srednia < 6) {
+            srednia += dodatkowe;
+        }
+
+        document.getElementsByClassName('srednia')[0].innerHTML = srednia;
 });
