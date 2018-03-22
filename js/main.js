2f6e9f3875b8cd7fb5294aa6ea4ee08705f6667e
Sebastian Śliwiński <sxsliwinski@gmail.com>
2018-03-22 21:10:53 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 document.getElementById('oblicz').addEventListener('click', function () {
-    var uczen = document.getElementsByClassName('uczen');
-    var tytul = document.querySelectorAll('[type="number"]');
-    var wynik = 0;
+    var uczniowie = document.getElementsByClassName('uczniowie');
     var dodatkowe = 0.5;
-    
-        for (var i = 0; i < 7; i++) {
-            var liczba = parseFloat(tytul[i].value);
+
+    for (var i = 0; i < uczniowie.length; i++) {
+        var tytul = document.querySelectorAll(`#uczen${i+1} [type=number]`);
+        var wynik = 0;
+
+        for (var x = 0; x < tytul.length; x++) {
+            var liczba = parseFloat(tytul[x].value);
             var srednia = (wynik += liczba) / 7;
+            if(liczba == 1){
+                document.getElementsByClassName('uczen')[i].style.backgroundColor = 'red';
+            }
         }
-        if (document.getElementsByClassName('zajecia-dodatkowe') !== '' && srednia < 6) {
+        if (document.getElementsByClassName('zajecia-dodatkowe')[i].value !== '' && srednia < 6) {
             srednia += dodatkowe;
         }
 
-        document.getElementsByClassName('srednia')[0].innerHTML = srednia;
+        document.getElementsByClassName('srednia')[i].innerHTML = srednia.toFixed(2);
+
+        if (document.getElementsByClassName('srednia')[i].innerHTML >= 4.75) {
+            document.getElementsByClassName('uczen')[i].style.backgroundColor = 'green';
+        } 
+    } 
 });
